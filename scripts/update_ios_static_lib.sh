9bd3106860d961c5e16fa14d9af78ed93688f134
Pedro Jorquera <pjorquera@okode.com>
2021-07-08 19:25:22 +0200

    Added local iOS library

--- scripts/update_ios_static_lib.sh
+++ scripts/update_ios_static_lib.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
+cd prebuilt-ios-lib
+echo "Airship - Unzipping prebuilt iOS library (version 9.4.0)"
+unzip Airship-iOS-9.4.0.zip
+echo "Airship - Moving prebuilt iOS library to src/ios"
+mv -f Airship ../src/ios
+echo "Airship - Done"
+cd -
 
-VERSION=9.4.0
-
-echo "Downloading libUAirship-$VERSION.zip from bintray..."
-curl -s -LO "https://urbanairship.bintray.com/iOS/urbanairship-sdk/$VERSION/libUAirship-$VERSION.zip"
-echo "Unzipping libUAirship into temp directory $SCRIPT_DIRECTORY/temp..."
-unzip -q -d temp libUAirship-$VERSION.zip
-echo "Making room for Airship directory in src/ios/..."
-rm -rf src/ios/Airship
-echo "Moving Airship directory to src/ios/..."
-mv -f temp/Airship src/ios/
-echo "Cleaning up..."
-rm -rf temp
-rm -rf libUAirship-$VERSION.zip
-echo "Done"
